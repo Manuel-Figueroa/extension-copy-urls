17d155217a0f1f0e58598a3528e0739674f85966
Manuel-Figueroa <figueroagarciamanuel@gmail.com>
2023-05-27 23:42:11 -0600

    Get urls by web page

--- scrip.js
+++ scrip.js
@@ -1,36 +1,71 @@
-const boton= document.getElementById("btn-urls"),
+const btn_all= document.getElementById("btn-urls"),
 btn_modal=document.getElementById('btn-modal'),
 btn_close=document.getElementById('btn-close'),
-btn_sl=document.getElementById('btn-urls-sl');
+btn_sl=document.getElementById('btn-urls-sl'),
+btn_pages=document.getElementById('btn-pages');
 
-async function viewElements() {
-    const container=document.getElementById('cuerpo'),
-    tab = await chrome.tabs.query({}),
-    div2=document.createElement('div'),
-    cuadro=document.getElementById('cuadro');
-
-    div2.style.height='4cm';
-    div2.style.width='8cm';
-    div2.style.overflow='auto';
-    div2.setAttribute('id','temp');
-    select_all_option(div2);
-    for(let i=0; i< tab.length; i++){
-        const check = document.createElement("input"),
-        label = document.createElement("span"),
-        br = document.createElement("br");
-
-        check.setAttribute('type','checkbox');
-        check.setAttribute('value',tab[i].url);
-        check.setAttribute('class','check');
-        label.textContent= tab[i].title.substring(0,20);
-        div2.appendChild(check);
-        div2.appendChild(label);
-        div2.appendChild(br);
-    }
-    container.appendChild(div2);
-    cuadro.style.height='5cm';
-    cuadro.style.width='10cm';
+let selection_type='',
+active_urls=[];
+function create_subcontainer(){
+    const container=document.getElementById('element-container'),
+    subcontainer=document.createElement('div'),
+    root_element=document.getElementById('root-element');
+    subcontainer.style.height='4cm';
+    subcontainer.style.width='8cm';
+    subcontainer.style.overflow='auto';
+    subcontainer.setAttribute('id','temp');
+    select_all_option(subcontainer);
+    container.appendChild(subcontainer);
+    root_element.style.height='5cm';
+    root_element.style.width='10cm';
+    
+    return subcontainer;
 }
+function create_rowCheck(subcontainer,text_row){
+    const check = document.createElement("input"),
+    label = document.createElement("span"),
+    br = document.createElement("br");
+
+    check.setAttribute('type','checkbox');
+    check.setAttribute('class','check');
+    label.textContent= text_row;
+    subcontainer.appendChild(check);
+    subcontainer.appendChild(label);
+    subcontainer.appendChild(br);
+    return  check;
+}
+async function viewElements() {
+    const tab = await chrome.tabs.query({}),
+    div2=create_subcontainer();
+    active_urls=[];
+    for(let i=0; i< tab.length; i++){
+        if(!active_urls.includes(tab[i].url)){
+            create_rowCheck(div2,tab[i].title.substring(0,20));
+            active_urls.push(tab[i].url);
+        }
+    }
+    
+    selection_type='u';
+}
+async function view_pages(){
+    const tab = await chrome.tabs.query({}),
+    div2=create_subcontainer();
+
+    let arr_pages=[];
+    for(let i=0; i< tab.length; i++){
+        const page_item=tab[i].url.split('://')[1].split('/')[0];
+        if(!arr_pages.includes(page_item))
+            arr_pages.push(page_item);
+        if(!active_urls.includes(tab[i].url))
+            active_urls.push(tab[i].url);
+    }
+    for(let i=0; i< arr_pages.length; i++){
+        const check = create_rowCheck(div2, arr_pages[i]);        
+        check.setAttribute('value',arr_pages[i]);
+    }
+    selection_type='p';
+}
+
 function select_all_option(node_parent) {
     const check = document.createElement("input"),
     label = document.createElement("span"),
@@ -43,10 +78,10 @@ function select_all_option(node_parent) {
     node_parent.appendChild(label);
     node_parent.appendChild(br);
     
-    check.addEventListener('click',( e )=> {
+    check.addEventListener('click',( {target} )=> {
         const checks=document.getElementsByClassName('check');
         for (let i = 0; i < checks.length; i++) 
-            checks[i].checked=e.target.checked;
+            checks[i].checked=target.checked;
         
     })
 }
@@ -59,25 +94,39 @@ function urls_sl(){
     txt_url.style.position = "absolute";
     txt_url.style.left = "-9999px";
     div.appendChild(txt_url);
-    for (let i = 0; i < checks.length; i++)
-            if(checks[i].checked){
-                txt_url.textContent += checks[i].value+"\n";
-                elements_active=true;
+    
+    for (let i = 0; i < checks.length; i++){
+        if(checks[i].checked){
+            switch (selection_type) {
+                case 'u':
+                    txt_url.textContent += active_urls[i]+"\n";
+                    elements_active=true;
+                    break;
+                case 'p':
+                    const result = active_urls.filter(url_i => url_i.includes(checks[i].value));
+                    result.forEach( element => txt_url.textContent += element+"\n");
+                    elements_active=true;
+                    break;
             }
-        txt_url.select();
-        document.execCommand("copy");
-        div.removeChild(txt_url);
-        btn_sl.style.backgroundColor=(elements_active)?"green":"red";
-        btn_sl.textContent=(elements_active)?"Listo":"No hay elementos seleccionados";
+        }
+    }
+
+    txt_url.select();
+    document.execCommand("copy");
+    div.removeChild(txt_url);
+    btn_sl.style.backgroundColor=(elements_active)?"green":"red";
+    btn_sl.textContent=(elements_active)?"Listo":"No hay elementos seleccionados";
 }
 function close_modal(){
     const temp_div=document.getElementById('temp'),
-    cuadro=document.getElementById('cuadro');
+    root_element=document.getElementById('root-element');
     temp_div.remove();
-    cuadro.style.height='0.9cm';
-    cuadro.style.width='5cm';
+    root_element.style.height='0.9cm';
+    root_element.style.width='5cm';
+    btn_sl.style.backgroundColor="steelblue";
+    btn_sl.textContent="Obtener solo seleccionados";
 }
-async function mov(){
+async function allUrls(){
     let queryOptions = {};
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
     const txt_url=document.createElement("textarea"),
@@ -93,12 +142,13 @@ async function mov(){
     txt_url.select();
     document.execCommand("copy");
     div.removeChild(txt_url);
-    boton.style.backgroundColor="green";
-    boton.textContent="Listo"
+    btn_all.style.backgroundColor="green";
+    btn_all.textContent="Listo"
 }
 
-boton.addEventListener('click',mov);
+btn_all.addEventListener('click',allUrls);
 btn_modal.addEventListener('click',viewElements);
 btn_close.addEventListener('click',close_modal);
+btn_pages.addEventListener('click',view_pages);
 btn_sl.addEventListener('click',urls_sl);
 
